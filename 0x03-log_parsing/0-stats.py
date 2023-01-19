9eac3a677bdeea0491ea3fa048414006c7069402
Aaron Abidemi <99512094+AaronAbidemi@users.noreply.github.com>
2023-01-19 23:04:11 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,50 @@
 #!/usr/bin/python3
-
-"""Script that reads stdin line by line and computes metrics"""
-
+"""
+Statistics calculation
+"""
 import sys
 
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+def printStatus(status_codes, total_size):
+    """ Prints information """
+    print('File size: {:d}'.format(total_size))
+    sorted_keys = sorted(status_codes.keys())
+    for k in sorted_keys:
+        if status_codes[k] > 0:
+            print('{}: {:d}'.format(k, status_codes[k]))
 
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
-
-count = 0
-size = 0
+stdin = sys.stdin
+status_codes = {'200': 0, '301': 0,
+                '400': 0, '401': 0,
+                '403': 0, '404': 0,
+                '405': 0, '500': 0
+                }
+total_size = 0
+loops = 0
 
 try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
-
-        stlist = line.split()
-        count += 1
-
+    for line in stdin:
+        if loops == 10:
+            printStatus(status_codes, total_size)
+            loops = 0
+        loops += 1
+        code = 0
+        size = 0
+        found = line.split(' ')
         try:
-            size += int(stlist[-1])
-        except:
+            size = int(found[-1])
+            total_size += size
+        except(Exception):
             pass
-
         try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
+            code = found[-2]
+            if code in status_codes:
+                status_codes[code] += 1
+        except(Exception):
             pass
-    printsts(sts, size)
+    printStatus(status_codes, total_size)
 
-
-except KeyboardInterrupt:
-    printsts(sts, size)
+except(KeyboardInterrupt):
+    printStatus(status_codes, total_size)
     raise
